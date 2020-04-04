f1da4edb290f1c05d4cdaf031098801efb0b1555
Captain Burrito <fr3akcs@hotmail.com>
2020-04-04 14:45:58 +0200

    First task done

--- aoc-2018-1/1.py
+++ aoc-2018-1/1.py
@@ -1,4 +1,23 @@
+def readfile():
+    
+    sum=0
+    
+    with open("1-input.txt") as f:
+        
+        for line in f.readlines():
+            
+            operator = line[0]
+            value = line[1:]
+            
+            if operator == "+":
+                sum=sum+int(value)
+            elif operator == "-":
+                sum=sum-int(value)
+        
+        print(sum)
+
 def main():
-    print("Witaj świecie!")
+    readfile()
+
 if __name__ == "__main__":
     main()
